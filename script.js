5256b88f029efd198cde736b14ff3da33f9d2551
q-tee-tamia-tee <nothandomhlongo709@gmail.com>
2025-09-27 11:57:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const button = document.getElementById('aboutButton');
+const textElement = document.getElementById('aboutText');
+
+button.addEventListener('click', function() {
+  // Toggle the 'hidden' class on the text element
+  textElement.classList.toggle('hidden');
+
+  // Optional: Change button text to reflect the current state
+});
+
+const g10_button = document.getElementById('g10_button');
+const g10_text = document.getElementById('g10_text')
+g10_button.addEventListener('click', function() {
+  // Toggle the 'hidden' class on the text element
+  g10_text.classList.toggle('hidden');
+
+  // Optional: Change button text to reflect the current state
+});
+
+const g11_button = document.getElementById('g11_button');
+const g11_text = document.getElementById('g11_text')
+g11_button.addEventListener('click', function() {
+  // Toggle the 'hidden' class on the text element
+  g11_text.classList.toggle('hidden');
+
+  // Optional: Change button text to reflect the current state
+});
+
+const g12_button = document.getElementById('g12_button');
+const g12_text = document.getElementById('g12_text')
+g12_button.addEventListener('click', function() {
+  // Toggle the 'hidden' class on the text element
+  g12_text.classList.toggle('hidden');
+
+  // Optional: Change button text to reflect the current state
+});
+
